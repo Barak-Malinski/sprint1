e1820f1c29d72431ad00c489e8bb309ffdbac1b7
Barak-Malinski <116379304+Barak-Malinski@users.noreply.github.com>
2025-01-28 20:48:06 +0200

    upload changes

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
 const MINES = '💣'
 const EMPTY = ''
 
-var gBoard = {
+var gBoard = [{
     minesAroundCount: 4,
     isShown: false,
-    isMine: false,
+    isMine: false, 
     isMarked: true
-}
+}]
 
 var gLevel = {
     SIZE: 4,
@@ -22,97 +22,137 @@ var gGame = {
     secPassed: 0
 }
 
+var minesCount
+
 function onInit(){
 
     const gBoard = buildBoard()
     console.table(gBoard)
     renderBoard(gBoard)
+    
 }
 
 function buildBoard(){
     //create matrix 4*4 with 2 MINES
     const board = []
-    for (var i = 0; i < 4 ; i++) {
+    for (var i = 0; i < gLevel.SIZE ; i++) {
         board[i] = []
-        for (var j = 0; j < 4 ; j++) {
-            board[i][j] = EMPTY
+        for (var j = 0; j < gLevel.SIZE ; j++) {
+            board[i][j] = gBoard
         }
     }
     board[1][1] = MINES
     board[2][2] = MINES
+    
+    
     return board
     //create matrix 8*8 with 14 MINES
     //create matrix 12*12 with 32 MINES
     
-}
 
-function setMinesNegsCount(board){}{
-    
 }
 
 
-// function renderBoard(board){
-//     var strHTML = ''
-//     for (var i = 0; i < board.length ; i++) {
-//         const row = board[i]
-//         strHTML += '<tr>'
-//         for (var j = 0; j < row.length ; j++) {
-//             var cell = row[j]
-//             var className = 'white'
-//             const tdId = `cell-${i}-${j}`
-//             strHTML += `<td id ="${tdId}"
-//                         onclick=onCellClicked(this)" 
-//                         class="${className}">
-//                             ${cell} 
-//                         </td>`
+// var count = countMinesAround(gBoard, 0, 0)
+// console.log('Found', count, 'mines around me')
+
+// function countMinesAround(board, rowIdx, colIdx){
+//     var minesCount = 0
+//     for (var i = rowIdx -1 ; i <= rowIdx +1 ; i++) {
+//         if(i < 0 || i >= board.length) continue
+//         for (var j = colIdx -1; j <= colIdx +1 ; j++) {
+//             if(i === rowIdx && j === colIdx) continue
+//             if(j < 0 || j >= board[0].length) continue
+//             var currCell = board[i][j]
+//             if(currCell === MINES) minesCount++
 //         }
-//         strHTML += '</tr>'
 //     }
-
-//     console.log(strHTML)
-
-//     const elBoard = document.querySelector('.board')
-//     elBoard.innerHTML = strHTML
-   
-//     console.log(elBoard)
-    
+//     return minesCount
 // }
 
-function renderBoard(board){
-    var strHTML = ''
-    for (var i = 0; i < board.length ; i++) {
-        strHTML += '<table>'
+// var bestPos = findBestPos(gBoard)
+// console.log('best pos', bestPos)
 
-        for (var j = 0; j < board[0].length ; j++) {
-           strHTML += `<td>${MINES}
-           </td>`     
-           console.log(strHTML) 
+// function findBestPos(board){
+//     var maxMinesCount = 0
+//     var bestPos = null
+//     for (var i = 0; i < board.length ; i++) {
+//         for (var i = 0; i < board[0].length ; i++) {
+//             if(board[i][j] === MINES) continue
+//             var count = countMinesAround(board, i, j)
+//             if(count > maxMinesCount) {
+//                 maxMinesCount = count
+//                 bestPos = { i:i, j:j }
+//             }
+//         }
+//     }
+//     return bestPos
+// }
+
+
+var count = setMinesNegsCount(gBoard)
+console.log('Found', count, 'mines around me')
+function setMinesNegsCount(board, rowId, colId){
+    
+    var minesCount = 0
+    
+    for (var i = rowId - 1; i <= rowId + 1; i++) {
+      
+        if (i < 0 || i > board.length - 1) continue 
+
+        for (var j = colId - 1; j <= colId + 1; j++) {
+            if (i === rowId && j === colId) continue
+            
+            if (j < 0 || j > board[0].length - 1) continue 
+                var currCell = board[i][j]
+            if (currCell === MINES){} minesCount++
         }
-
-        strHTML += '</table>'
     }
-
-    // console.log(strHTML)
-
-    const elBoard = document.querySelector('.board')
-    elBoard.innerHTML = strHTML
-   
-    // console.log(elBoard)
+    
+    return minesCount
     
 }
 
-function onCellClicked(elCell, i, j){
 
-}
-
-function onCellMarked(elCell){
-
-}
-
-function checkGameOver(){
-
-}
-
-function expandShown(board, elCell, i, j){
-
+    function renderBoard(board){
+                
+        var strHTML = '<table><tbody>'
+        for (var i = 0; i < board.length ; i++) {
+            
+            
+            strHTML += '<tr>'
+            for (var j = 0; j < board[0].length ; j++) {
+                
+                
+                const res = setMinesNegsCount(gBoard)
+                const cell = board[i][j]
+                const className = `cell cell-${i}-${j}`
+                strHTML += `<td class="${className}">${cell}</td>`  
+                
+            }
+            strHTML += '</tr>'
+        }
+        strHTML += '</tbody></table>'
+        
+        const elBoard = document.querySelector('.board')
+        elBoard.innerHTML = strHTML
+        
+      
+        
+    }
+    
+    function onCellClicked(elCell, i, j){
+        
+    }
+    
+    function onCellMarked(elCell){
+        
+    }
+    
+    function checkGameOver(){
+        
+    }
+    
+    function expandShown(board, elCell, i, j){
+        
 }
